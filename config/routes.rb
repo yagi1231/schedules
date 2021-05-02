c97f7077299cd295655bada82b568c0e258064a9
yagi1231 <at.sao.1231@gmail.com>
2021-05-02 12:00:13 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'act1/act1'
+  get 'schedules/index'
+  get 'schedules/index'
+  get 'schedule/index'
+
+  get 'lessons/hello'
+  resources :schedules
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
